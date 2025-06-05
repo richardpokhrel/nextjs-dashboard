cef65132d0c8a45b5d18249503157805927e996d
richardpokhrel <108170976+richardpokhrel@users.noreply.github.com>
2025-06-05 14:35:22 +0545

    updated

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,9 @@
 // app/ui/fonts.ts
-import { Lusitana } from 'next/font/google';
 
-export const lusitana = Lusitana({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-});
+import { Lusitana }  from "next/font/google";
+
+import { Inter } from "next/font/google";
+
+export const inter = Inter({ subsets: ['latin'], weight: '400' });
+
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "400", style: "normal", });
